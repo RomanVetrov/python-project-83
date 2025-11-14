8781f9c024bde5f72d4a8067b17f8ea02c452465
RomanVetrov <romanvikernes@gmail.com>
2025-11-14 21:56:48 +0300

    Обновление конфигурации запуска и сборки

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
-uv sync
-psql -a -d $DATABASE_URL -f database.sql
+source "$HOME/.local/bin/env"
+
+make install
+
+psql -a -d "$DATABASE_URL" -f database.sql
+
